5630f75ca99aaf7314f7231962b1649dbd22a356
Alishba526 <159262026+Alishba526@users.noreply.github.com>
2024-03-19 03:31:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: " enter first number", type: "number", name: "firstNumber" },
+  { message: " enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "devision"],
+  },
+]);
+console.log(answer);
+
+//condition statment;
+
+if (answer.operator === "additon") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "devision") {
+  console.log(answer.firstNumber/answer.secondNumber);
+} else {
+  console.log("please valid a number");
+}
